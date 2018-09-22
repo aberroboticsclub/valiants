239392256af53aaa907ea5ee5a6fbebbf2a1d8ca
tomosfearn <tfearn10@outlook.com>
2018-09-22 16:01:38 +0100

    updated test code

--- valiants-test/valiants-test.ino
+++ valiants-test/valiants-test.ino
@@ -1,6 +1,11 @@
 #include <valiant.h>
-
-const byte speaker = A3;
+#define TEST_MOTORS false
+#define TEST_ULTRASONIC false
+#define TEST_LED false
+#define TEST_LDR false
+#define TEST_SERVO false
+#define TEST_INFRARED false
+#define TEST_SPEAKER false
 
 Valiant valiant;
 
@@ -42,7 +47,8 @@ void setup() {
 }
 
 void loop() {
-  /*
+  #ifdef TEST_MOTORS
+  if (TEST_MOTORS) {
   //test motors HIGH and LOW
    valiant.forwards();
    delay(1000);
@@ -53,51 +59,79 @@ void loop() {
    valiant.reverse();
    delay(1000);
    valiant.halt();
-   delay(5000);*/
+   delay(5000);
+  }
+  #endif
 
-
+  #ifdef TEST_ULTRASONIC
+  if (TEST_ULTRASONIC) {
   //test ultrasonics
-  //Serial.println(valiant.ping_ultrasonic_cm());
+  Serial.println(valiant.ping_ultrasonic_cm());
+  }
+  #endif
 
-
-/*
+  #ifdef TEST_LED
+  if (TEST_LED) {
   //test LED
 	valiant.turnOnLed();
 	delay(1000);
 	valiant.turnOffLed();
-	delay(1000);*/
+	delay(1000);
+  }
+  #endif
 
+  #ifdef TEST_SERVO
+  if (TEST_SERVO) {
   //test servo
-  //valiant.turretMove(90);
-  //delay(1000);
+  valiant.turretMove(90);
+  delay(1000);
+  valiant.turretMove(60);
+  delay(1000);
+  valiant.turretMove(140);
+  delay(1000);
+  valiant.turretMove(90);
+  delay(5000);
+  }
+  #endif
 
+  #ifdef TEST_LDR
+  if (TEST_LDR) {
   //test LDR
-	//Serial.println(valiant.readLight());
-	//delay(50);
+	Serial.println(valiant.readLight());
+	delay(50);
+  }
+  #endif
 
-  /*
+  #ifdef TEST_INFRARED
+  if (TEST_INFRARED) {
   //test Infrared
   Serial.print("left ");
   Serial.print(valiant.readLeftInfrared());
   Serial.print(" | ");
   Serial.print("right ");
   Serial.println(valiant.readRightInfrared());
-  delay(10);*/
+  delay(10);
+  }
+  #endif
 
+  #ifdef TEST_SPEAKER
+  if (TEST_SPEAKER) {
 	// iterate over the notes of the melody:
-  /*for (int thisNote = 0; thisNote < 45; thisNote++) {
+  for (int thisNote = 0; thisNote < valiant.calculateLength(melody); thisNote++) {
 
     // to calculate the note duration, take one second
     // divided by the note type.
     //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
     int noteDuration = 1000 / noteDurations[thisNote];
-    tone(speaker, melody[thisNote], noteDuration);
+    tone(valiant.speaker, melody[thisNote], noteDuration);
 
     // to distinguish the notes, set a minimum time between them.
     // the note's duration + 30% seems to work well:
     int pauseBetweenNotes = noteDuration * 1.30;
     delay(pauseBetweenNotes);
     // stop the tone playing:
-    noTone(speaker);
-  }*/
+    noTone(valiant.speaker);
+  }
+  }
+  #endif
 }
